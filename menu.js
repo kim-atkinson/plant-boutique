7c85bc2e648608a994ef754606b640fdd043d368
kim-atkinson <kimfromtoronto@gmail.com>
2023-03-17 10:26:53 -0400

    Added transition and close states to menu.

--- menu.js
+++ menu.js
@@ -2,7 +2,16 @@ const toggleTag = document.querySelector("a.toggle-nav")
 const mainTag = document.querySelector("main")
 
 // When I click the toggle tag, toggle a calss of open on the main tag
-
+// If it's open make the toggle tag say closed
+// If not make the toggle tag say menu
 toggleTag.addEventListener("click", function () {
+    // classList returns the CSS properties with same class name
+    // To get the main section to open/move has to do with the look and feel - and that has to do with the CSS
     mainTag.classList.toggle("open");
-})
+
+    if (mainTag.classList.contains("open")) {
+        toggleTag.innerHTML = `<img src="assets/close.svg"> Close`
+    } else {
+        toggleTag.innerHTML = `<img src="assets/menu.svg"> Menu`
+    }
+}) 
