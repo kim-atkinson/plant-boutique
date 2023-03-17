6b75903d79a163aaf54c0d2416c370adfdef99c4
kim-atkinson <kimfromtoronto@gmail.com>
2023-03-16 23:16:53 -0400

    Created toggle interaction on menu.

---
+++ menu.js
@@ -0,0 +1,8 @@
+const toggleTag = document.querySelector("a.toggle-nav")
+const mainTag = document.querySelector("main")
+
+// When I click the toggle tag, toggle a calss of open on the main tag
+
+toggleTag.addEventListener("click", function () {
+    mainTag.classList.toggle("open");
+})
