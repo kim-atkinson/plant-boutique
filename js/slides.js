1897c6c1933bb857ed20a87e8836ac353dcc9a9b
kim-atkinson <kimfromtoronto@gmail.com>
2023-03-19 10:52:36 -0400

    Added snapping grid effect to slideshow.

--- js/slides.js
+++ js/slides.js
@@ -28,9 +28,21 @@ slideArea.addEventListener("click", function () {
 // When mouse goes over the slude area, the images will go in a random place
 slideArea.addEventListener("mouseover", function () {
     images.forEach((image) => {
-        const x = 100 * Math.random() - 50
-        const y = 100 * Math.random() - 50
+        const x = 25 * (Math.floor(Math.random() * 5)) - 50
+        const y = 25 * (Math.floor(Math.random() * 5)) - 50
 
         image.style.transform = `translate(${x}px, ${y}px)`
     })
 })
+
+// When mouse moves away, put the images back. 
+slideArea.addEventListener("mouseout", function () {
+    images.forEach(image =>{
+        image.style.transform = ""
+    })
+})
+
+
+
+
+
